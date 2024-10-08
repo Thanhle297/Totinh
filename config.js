4c51045e359a6ed50359fc0e51d62244a884de16
Thanhle297 <stu725102086@hnue.edu.vn>
2024-10-09 00:22:19 +0700

    your commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Cậu ấn vô đây nè !',
+    title: 'Phải chăng em đã làm tôi yêu em ngay từ cái nhìn đầu tiên 😙',
+    // desc: 'Phải chăng em đã làm tôi say ngay từ lúc thấy nụ cười ấy ',
+    desc:'Làm người yêu tớ nhé',
+    btnYes: 'Yes <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu tôi <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100061354991417'
+}
