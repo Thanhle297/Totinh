7a8957cda433ab1aba2641b1463d65c0888d9418
Thanhle297 <stu725102086@hnue.edu.vn>
2024-10-15 11:26:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường.
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Gió đông sắp về rồi.Tớ không muốn một mình cô đơn nữa.Cậu ak cậu sẽ làm ngọn lửa của tớ chứ.Đằng sau chữ yêu đây là thương.`,
     btnIntro: 'Cậu ấn vô đây nè !',
     title: 'Phải chăng em đã làm tôi yêu em ngay từ cái nhìn đầu tiên 😙',
     // desc: 'Phải chăng em đã làm tôi say ngay từ lúc thấy nụ cười ấy ',
